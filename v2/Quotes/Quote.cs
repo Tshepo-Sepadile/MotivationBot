30af3bd0d5233788918f4cdb317bda01da67179d
Tshepo-Sepadile <tsheposepadile1995@gmail.com>
2024-03-10 14:21:16 +0200

    Remove unused code

--- v2/Quotes/Quote.cs
+++ v2/Quotes/Quote.cs
@@ -8,17 +8,6 @@ namespace MotivationBot.v2.Quotes
     {
         public string Author { get; set; }
         public string Text { get; set; }
-
-        //public Quote(string author, string text)
-        //{
-        //    if (string.IsNullOrEmpty(author))
-        //        throw new ArgumentNullException(nameof(author));
-        //    if (string.IsNullOrEmpty(text))
-        //        throw new ArgumentNullException(nameof(text));
-        //    Author = author;
-        //    Text = text;
-        //}
-
         public override string ToString() =>
             $"\"{Text}\" - {Author}";
     }
